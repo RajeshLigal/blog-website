31b1b4db2110f9cf90c21f438f03abc209979163
Rajesh Ligal <rajeshligal21@gmail.com>
2020-08-22 16:20:58 +0545

    Connects Mongo Local, Adds Functionality Compose

--- app.js
+++ app.js
@@ -3,38 +3,67 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const mongoose = require("mongoose");
 const _ = require("lodash");
 
-const homeStartingContent =
-  "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
-const aboutContent =
-  "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
-const contactContent =
-  "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+// const aboutContent =
+//   "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
+// const contactContent =
+//   "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
 const app = express();
 
 app.set("view engine", "ejs");
-
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-let posts = [];
+mongoose.connect("mongodb://localhost:27017/blogWebsiteDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const postsSchema = {
+  title: String,
+  content: String,
+};
+
+const Post = mongoose.model("Post", postsSchema);
+
+const post1 = new Post({
+  title: "Home",
+  content:
+    "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.",
+});
+
+const defaultPosts = [post1];
 
 //HOME ROUTE
 app.get("/", function (req, res) {
-  res.render("home", { startingContent: homeStartingContent, posts: posts });
+  Post.find({}, function (err, foundPosts) {
+    if (foundPosts.length === 0) {
+      Post.insertMany(defaultPosts, function (err) {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log("successfully added");
+        }
+      });
+      res.redirect("/");
+    } else {
+      res.render("home", { posts: foundPosts });
+    }
+  });
 });
 
-//CONTACT ROUTE
-app.get("/contact", function (req, res) {
-  res.render("contact", { contactContent: contactContent });
-});
+// //CONTACT ROUTE
+// app.get("/contact", function (req, res) {
+//   res.render("contact", { contactContent: contactContent });
+// });
 
-//ABOUT ROUTE
-app.get("/about", function (req, res) {
-  res.render("about", { aboutContent: aboutContent });
-});
+// //ABOUT ROUTE
+// app.get("/about", function (req, res) {
+//   res.render("about", { aboutContent: aboutContent });
+// });
 
 //COMPOSE ROUTE
 app.get("/compose", function (req, res) {
@@ -42,28 +71,31 @@ app.get("/compose", function (req, res) {
 });
 
 app.post("/compose", function (req, res) {
-  const post = {
-    title: req.body.postTitle,
-    content: req.body.postBody,
-  };
-  posts.push(post);
+  const inputTitle = req.body.postTitle;
+  const inputBody = req.body.postBody;
+
+  const post = new Post({
+    title: inputTitle,
+    content: inputBody,
+  });
+  post.save();
   res.redirect("/");
 });
 
-app.get("/posts/:postName", function (req, res) {
-  const requestTitle = _.lowerCase(req.params.postName);
+// app.get("/posts/:postName", function (req, res) {
+//   const requestTitle = _.lowerCase(req.params.postName);
 
-  posts.forEach(function (post) {
-    const storedTitle = _.lowerCase(post.title);
+//   posts.forEach(function (post) {
+//     const storedTitle = _.lowerCase(post.title);
 
-    if (requestTitle === storedTitle) {
-      res.render("post", {
-        title: post.title,
-        content: post.content,
-      });
-    }
-  });
-});
+//     if (requestTitle === storedTitle) {
+//       res.render("post", {
+//         title: post.title,
+//         content: post.content,
+//       });
+//     }
+//   });
+// });
 
 app.listen(3000, function () {
   console.log("Server started on port 3000");
