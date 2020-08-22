8f3311327681942593d0aa93ca790b9acdf6365d
Rajesh Ligal <rajeshligal21@gmail.com>
2020-08-22 16:47:15 +0545

    Adds Routing

--- app.js
+++ app.js
@@ -6,10 +6,10 @@ const ejs = require("ejs");
 const mongoose = require("mongoose");
 const _ = require("lodash");
 
-// const aboutContent =
-//   "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
-// const contactContent =
-//   "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+const aboutContent =
+  "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
+const contactContent =
+  "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
 const app = express();
 
@@ -55,15 +55,15 @@ app.get("/", function (req, res) {
   });
 });
 
-// //CONTACT ROUTE
-// app.get("/contact", function (req, res) {
-//   res.render("contact", { contactContent: contactContent });
-// });
+//CONTACT ROUTE
+app.get("/contact", function (req, res) {
+  res.render("contact", { contactContent: contactContent });
+});
 
-// //ABOUT ROUTE
-// app.get("/about", function (req, res) {
-//   res.render("about", { aboutContent: aboutContent });
-// });
+//ABOUT ROUTE
+app.get("/about", function (req, res) {
+  res.render("about", { aboutContent: aboutContent });
+});
 
 //COMPOSE ROUTE
 app.get("/compose", function (req, res) {
@@ -82,20 +82,33 @@ app.post("/compose", function (req, res) {
   res.redirect("/");
 });
 
-// app.get("/posts/:postName", function (req, res) {
-//   const requestTitle = _.lowerCase(req.params.postName);
+app.get("/posts/:postName", function (req, res) {
+  const customTitle = _.capitalize(req.params.postName);
 
-//   posts.forEach(function (post) {
-//     const storedTitle = _.lowerCase(post.title);
+  Post.findOne({ title: customTitle }, function (err, foundPost) {
+    if (!err) {
+      if (!foundPost) {
+        res.redirect("/");
+      } else {
+        res.render("post", {
+          title: foundPost.title,
+          content: foundPost.content,
+        });
+      }
+    }
+  });
 
-//     if (requestTitle === storedTitle) {
-//       res.render("post", {
-//         title: post.title,
-//         content: post.content,
-//       });
-//     }
-//   });
-// });
+  // posts.forEach(function (post) {
+  //   const storedTitle = _.lowerCase(post.title);
+
+  //   if (customTitle === storedTitle) {
+  //     res.render("post", {
+  //       title: post.title,
+  //       content: post.content,
+  //     });
+  //   }
+  // });
+});
 
 app.listen(3000, function () {
   console.log("Server started on port 3000");
